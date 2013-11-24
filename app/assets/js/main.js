0066e3a30b3d0d4a11a144b7ba330bf7e6f13372
Matt Pardee <matt.pardee@gmail.com>
2013-11-23 19:40:05 -0800

    Moved things around

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -8,13 +8,13 @@ requirejs.config({
 
 requirejs.onError = function (err) {
 
-    var errorMsg = ['<p>There was an issue loading an app or dependency:</p><p>',
+    var errorMsg = ['<p>Oh boy. There was an issue loading an app or dependency.</p><p>',
         err.message, '</p><p>Require.js error type: ', err.requireType, '</p><p>',
-        'Please file a bug on the \
-            <a href="http://github.com/mattpardee/drop/issues" target="_blank">\
-            GitHub</a> repo. Afer closing this dialog you can switch to another application.</p>'].join('');
+        'Please copy the above text and file a bug on the \
+        <a href="http://github.com/mattpardee/drop/issues" target="_blank">\
+        GitHub</a> repo. Afer closing this dialog you can switch to another application.</p>'].join('');
 
-    $('#error-modal .modal-header h4').text('Require.js Error');
+    $('#error-modal .modal-header h4').text('Loading Error');
     $('#error-modal .modal-body').html(errorMsg);
 
     $('#error-modal').modal('show');
